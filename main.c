b7ebb982f86ed8f94458d3f2da1ffc962517acfe
shya <dan119@bk.ru>
2026-05-04 15:59:01 +0300

    исправлен dyn_array_append

--- main.c
+++ main.c
@@ -1,14 +1,16 @@
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 typedef struct {
-    size_t **data;
+    size_t *data;
     size_t length;
 } dyn_array; // (dynamic array)
 
 dyn_array dyn_array_init(size_t length){
     dyn_array array;
     if(length == 0){
+        array.data = 0;
         array.length=0;
         return array;
     }
@@ -19,17 +21,27 @@ dyn_array dyn_array_init(size_t length){
 
 void din_array_free(dyn_array *array){
     free(array->data);
-    array->length;
+    array->length=0;
 }
 
 void dyn_array_append(dyn_array *array, size_t element){
-    array->length++;
-    array->data = realloc(array->data, array->length);
-    array->data[array->length-1] = element;    
+    if (array->length) {
+        array->length++;
+        array->data = realloc(array->data, array->length);
+        array->data[array->length-1] = element;
+    }
+    else{
+        array->length++;
+        array->data = malloc(sizeof(size_t));
+        *array->data = element;
+    }
 }
 
 int main(){
     dyn_array array = dyn_array_init(0);
-
+    dyn_array_append(&array, 5);
+    dyn_array_append(&array, 10);
+    printf("%d %d\n", array.data[0], array.data[1]);
+    din_array_free(&array);
     return 0;
 }
