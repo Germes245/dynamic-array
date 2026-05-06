cc082389ab743b2ee6411a1f464832868b11d29d
shya <dan119@bk.ru>
2026-05-06 21:59:53 +0300

    временный для отладки код убран

--- src/add.c
+++ src/add.c
@@ -27,10 +27,6 @@ void dyn_array_insert(dyn_array *array, size_t index, size_t element){
         }
         array->length++;
         array->data == realloc(array->data, array->length);
-        for(size_t i = 0; i < array->length; i++){
-            printf("%d ", array->data[i]);
-        }
-        putchar('\n');
         size_t pointer_for_source = array->data + index*sizeof(size_t);
         memmove(pointer_for_source + sizeof(size_t), pointer_for_source, array->length - index - sizeof(size_t));
         array->data[index] = element;
