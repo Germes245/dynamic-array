b52f21f7155525c6a72f31254f04a4d7f1a6f257
shya <dan119@bk.ru>
2026-05-25 20:55:59 +0300

    модифицирование теста под общий шаблон для всех типов

--- tests/test1.c
+++ tests/test1.c
@@ -1,4 +1,4 @@
-#include <dyn_array.h>
+#include <dyn_array_{{prefix}}.h>
 #include <assert.h>
 #include <stddef.h>
 #include <stdlib.h>
@@ -6,36 +6,37 @@
 #include <stdint.h>
   
 int main(){
-    dyn_array array = dyn_array_init(0);
+    dyn_array_{{prefix}} array = dyn_array_{{prefix}}_init(0);
     assert(array.data == 0);
 
-    dyn_array_append(&array, 5);
+    dyn_array_{{prefix}}_append(&array, 5);
     uint8_t is_correct;
-    size_t el = dyn_array_get_element(array, 0, &is_correct);
+    {{type}} el = dyn_array_{{prefix}}_get_element(array, 0, &is_correct);
     assert(array.data[0] == 5 && el == 5 && is_correct == 1);
 
-    dyn_array_append(&array, 10);
-    el = dyn_array_get_element(array, 1, &is_correct);
+    dyn_array_{{prefix}}_append(&array, 10);
+    //dyn_array_{{prefix}}_get_element(array, 10);
+    el = dyn_array_{{prefix}}_get_element(array, 1, &is_correct);
     assert(array.data[1] == 10 && el == 10 && is_correct == 1);
 
-    dyn_array_get_element(array, 10, &is_correct);
+    dyn_array_{{prefix}}_get_element(array, 10, &is_correct);
     assert(is_correct == 0);
 
     printf("проба на double-free\n");
-    dyn_array_free(&array);
-    dyn_array_free(&array);
+    dyn_array_{{prefix}}_free(&array);
+    dyn_array_{{prefix}}_free(&array);
     printf("прошло\n\n");
     assert(array.data == 0 && array.length == 0);
 
     printf("проба на use-after-free\n");
-    dyn_array_append(&array, 1);
-    dyn_array_append(&array, 25);
-    dyn_array_append(&array, 50);
+    dyn_array_{{prefix}}_append(&array, 1);
+    dyn_array_{{prefix}}_append(&array, 25);
+    dyn_array_{{prefix}}_append(&array, 50);
     printf("прошло\n\n");
 
     printf("проба на buffer overflow\n");
 
-    dyn_array_get_element(array, 10, &is_correct);
+    dyn_array_{{prefix}}_get_element(array, 10, &is_correct);
     if(!is_correct) printf("понял что что-то нечисто: is_correct = %d\n", is_correct);
     printf("прошло\n\n");
     printf("insert\nбыло: ");
@@ -43,7 +44,7 @@ int main(){
         printf("%d ", array.data[i]);
     }
     putchar('\n');
-    dyn_array_insert(&array, 0, 1);
+    dyn_array_{{prefix}}_insert(&array, 0, 1);
     printf("стало: ");
     for(size_t i = 0; i < array.length; i++){
         printf("%d ", array.data[i]);
@@ -55,7 +56,7 @@ int main(){
         printf("%d ", array.data[i]);
     }
     putchar('\n');
-    dyn_array_delete(&array, 1, &is_correct);
+    dyn_array_{{prefix}}_delete(&array, 1, &is_correct);
     printf("стало: ");
     for(size_t i = 0; i < array.length; i++){
         printf("%d ", array.data[i]);
